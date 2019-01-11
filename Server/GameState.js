9b14517bdfc884f9f0027677121e9223bca1748c
shahid <shahid@linasmatkasse.se>
2019-01-11 19:23:03 +0100

    classify game state

--- Server/GameState.js
+++ Server/GameState.js
@@ -1,3 +1,4 @@
+/* eslint-disable class-methods-use-this */
 // const events = require('./events.js');
 
 /* eslint no-unused-vars: ["error", { "argsIgnorePattern": "game" }] */
@@ -22,81 +23,90 @@ function stopLoading(LoadingCtrl) {
   process.stdout.write('loading done! \n');
 }
 
-function AbstractGameState() {
-  this.initialized = false;
-  this.execute = (game) => {
+class AbstractGameState {
+  constructor() {
+    this.initialized = false;
+  }
+
+  execute(game) {
     console.log('You should never see this execute message :( \n, fix it! ');
-  };
-  this.setup = (game) => {
+  }
+
+  setup(game) {
     console.log('You should never see this setup message :( \n, fix it! ');
     // return this.execute();
-  };
-  this.handleEvents = (game) => {
+  }
+
+  handleEvents(game) {
     console.log('You should never see this events message :( \n, fix it! ');
-  };
-  this.update = (game) => {
+  }
+
+  update(game) {
     console.log('You should never see this updating message :( \n, fix it! ');
-  };
-  this.render = (game) => {
+  }
+
+  render(game) {
     console.log('You should never see this rendering message :( \n, fix it! ');
-  };
+  }
 }
 
-function InitGameState() {
-  this.execute = game => new Promise((resolve) => {
-    // Start loading animation
-    const loadingControl = startLoading();
-    process.stdout.write('Initializing game...\n');
-    setTimeout(() => {
-      // Stop loading animation
-      stopLoading(loadingControl);
+class InitGameState extends AbstractGameState {
+  execute(game) {
+    return new Promise((resolve) => {
+      // Start loading animation
+      const loadingControl = startLoading();
+      process.stdout.write('Initializing game...\n');
+      setTimeout(() => {
+        // Stop loading animation
+        stopLoading(loadingControl);
+        process.stdout.clearLine();
+        process.stdout.cursorTo(0);
+        process.stdout.write('Initializing done!\n');
+        resolve('init resolved');
+      }, 3000);
+      // reject('true');
+    });
+  }
+
+  handleEvents(game) {
+    return new Promise((resolve) => {
+      // for (let event in game.events) {
+      //     switch (event.event) {
+      //         case events.CHANGE_STATE:
+      //             game.setState(event.entity);
+      //             break;
+      //     }
+      // }
+      this.execute().then(() => {
+        resolve('handle envents resolved');
+      });
+    });
+  }
+}
+
+class MenuGameState extends AbstractGameState {
+  execute(game) {
+    return new Promise(resolve => setTimeout(() => {
       process.stdout.clearLine();
       process.stdout.cursorTo(0);
-      process.stdout.write('Initializing done!\n');
-      resolve('init resolved');
-    }, 3000);
-    // reject('true');
-  });
-
-  this.handleEvents = game => new Promise((resolve) => {
-    // for (let event in game.events) {
-    //     switch (event.event) {
-    //         case events.CHANGE_STATE:
-    //             game.setState(event.entity);
-    //             break;
-    //     }
-    // }
-    this.execute().then(() => {
-      resolve('handle envents resolved');
-    });
-  });
+      process.stdout.write('you are in the menu :)\n');
+      return resolve('menu resolved');
+    }, 4000));
+  }
 }
-InitGameState.prototype = new AbstractGameState();
 
-function MenuGameState() {
-  this.execute = game => new Promise(resolve => setTimeout(() => {
-    process.stdout.clearLine();
-    process.stdout.cursorTo(0);
-    process.stdout.write('you are in the menu :)\n');
-    return resolve('menu resolved');
-  }, 4000));
-}
-MenuGameState.prototype = new AbstractGameState();
-
-function PlayGameState() {
-  this.execute = (game) => {
+class PlayGameState extends AbstractGameState {
+  execute(game) {
     console.log('starting now...');
-  };
+  }
 }
 
-PlayGameState.prototype = new AbstractGameState();
 
-function FinishGameState() {
-  this.execute = (game) => {
+class FinishGameState extends AbstractGameState {
+  execute(game) {
     console.log('you ar dead! :( Ending game!');
-  };
+  }
 }
-FinishGameState.prototype = new AbstractGameState();
 
 module.exports = {
   init: InitGameState,
