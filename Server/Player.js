7d9a8559bd4f1cf223ef713666e4015dcff845b8
shahid <shahid@linasmatkasse.se>
2019-01-11 19:26:48 +0100

    classify palyer

--- Server/Player.js
+++ Server/Player.js
@@ -2,27 +2,30 @@ const UUID = require('node-uuid');
 // const Entity = require('./Entity.js');
 // const Shapes = require('./Shapes.js');
 
-const Player = (name, options = {}) => {
-  // console.log(options.shape.constructor.name, options.shape);
-  const uuid = UUID();
-  // Set up basic player
-  this.name = name;
-  this.id = uuid;
-  this.rad = 10;
-  this.color = 'red';
+class Player {
+  constructor(name, options = {}) {
+    // console.log(options.shape.constructor.name, options.shape);
+    const uuid = UUID();
+    // Set up basic player
+    this.name = name;
+    this.id = uuid;
+    this.rad = 10;
+    this.color = 'red';
 
-  this.shape = typeof options.shape === 'object' ? 'options.shape' : 'yes'; // new Shapes.Circ(10, new Shapes.Vect(20, 20));
+    this.shape = typeof options.shape === 'object' ? 'options.shape' : 'yes'; // new Shapes.Circ(10, new Shapes.Vect(20, 20));
+  }
 
   // console.log('-------------------------------------', this);
   // Set initial palyer state
   // this.currentState = new PlayerState.init(this);
 
-  this.setState = (newState) => {
+  setState(newState) {
     this.currentState = newState;
-  };
-  this.setSocket = (socketId) => {
+  }
+
+  setSocket(socketId) {
     this.socketId = socketId;
-  };
-};
+  }
+}
 
 module.exports = Player;
